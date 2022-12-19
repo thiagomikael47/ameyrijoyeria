1eeab14bdb8b6f3e09d2328d28188389de6ace40
unknown <mikaeljara47@gmail.com>
2022-12-18 22:40:37 -0500

    Proyecto ameyri

---
+++ js/dark-mode.js
@@ -0,0 +1,71 @@
+const darkMode = document.getElementById('dark-mode')
+const lightMode = document.getElementById('light-mode')
+
+darkMode.addEventListener('click', () => {
+  document.body.classList.toggle('dark__mode')
+  darkMode.classList.toggle('hide')
+  lightMode.classList.remove('hide')
+})
+
+lightMode.addEventListener('click', () => {
+  document.body.classList.remove('dark__mode')
+  darkMode.classList.toggle('hide')
+  darkMode.classList.remove('hide')
+})
+
+// botoonnnn
+(function(){
+  const listElements = document.querySelectorAll('.menu__item--show');
+  const list = document.querySelector('.menu__links');
+  const menu = document.querySelector('.menu__hamburguer');
+
+  const addClick = ()=>{
+      listElements.forEach(element =>{
+          element.addEventListener('click', ()=>{
+
+              
+              let subMenu = element.children[1];
+              let height = 0;
+              element.classList.toggle('menu__item--active');
+
+
+              if(subMenu.clientHeight === 0){
+                  height = subMenu.scrollHeight;
+              }
+
+              subMenu.style.height = `${height}px`;
+
+          });
+      });
+  }
+
+  const deleteStyleHeight = ()=>{
+      listElements.forEach(element=>{
+
+          if(element.children[1].getAttribute('style')){
+              element.children[1].removeAttribute('style');
+              element.classList.remove('menu__item--active');
+          }
+
+      });
+  }
+
+
+  window.addEventListener('resize', ()=>{
+      if(window.innerWidth > 800){
+          deleteStyleHeight();
+          if(list.classList.contains('menu__links--show'))
+              list.classList.remove('menu__links--show');
+
+      }else{
+          addClick();
+      }
+  });
+
+  if(window.innerWidth <= 800){
+      addClick();
+  }
+
+  menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
+
+})();
